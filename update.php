e50f897f591684e6d2d3c36fa851069cc00fe6d0
shantinathjain <snjain16@gmail.com>
2022-02-16 15:29:46 +0530

    modified

--- update.php
+++ update.php
@@ -123,7 +123,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Update Record</title>
+    <title>Update record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
